f46a1c483ff8e41fea976b1dc335e0ddf66ea9d6
jerlibgnzlz <jerlibgv@gmail.com>
2024-01-22 20:45:01 -0300

    feature nosotros+styles

--- app/nosotros/page.jsx
+++ app/nosotros/page.jsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link"
+import styles from "../style/nosotros.module.css"
 
 export const metadata = {
 
@@ -9,8 +12,30 @@ const PageNosotros = () => {
     return (
 
 
-        <h1>Desde Nosotros</h1>
 
+        <main className="contenedor">
+            <h2 className="heading">Nosotros</h2>
+
+            <div className={styles.contenido}>
+
+                <Image src="/img/nosotros.jpg"
+                    width={1000}
+                    height={800}
+                    alt="Imagen sobre nosotros" />
+
+
+                <div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius rhoncus ex, vitae pellentesque nisl congue sit amet. Vivamus dignissim sollicitudin lorem, sit amet porta leo sagittis at. Morbi at pellentesque leo. Sed finibus quam ut est bibendum, eget pellentesque eros feugiat.
+                    </p>
+
+                    <p>
+                        Vestibulum erat risus, sagittis eu tortor at, scelerisque tempor dui. Praesent sit amet sapien placerat, venenatis tellus quis, euismod enim. Morbi imperdiet justo non sollicitudin semper. In aliquet vulputate laoreet. Aenean pharetra nunc ex, ut bibendum risus luctus eu. Nullam eget ex massa. In sagittis consectetur erat nec fermentum. In ultrices.
+                    </p>
+
+                </div>
+            </div>
+        </main>
     )
 }
 
